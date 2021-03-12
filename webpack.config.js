a1f8bf32d4d443713503f70d4d952ecde72a00bc
Santiago <santiagoln09@gmail.com>
2021-03-11 20:36:51 -0500

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,22 @@ const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
-    output:{
-        path: path.resolve(__dirname,'dist'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
     },
-    resolve:{
-        extensions:['.js']
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     }
 }
